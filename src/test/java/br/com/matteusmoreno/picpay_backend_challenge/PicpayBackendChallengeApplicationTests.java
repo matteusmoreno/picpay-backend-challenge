7fe49b44e2a81cdad08538364137c3d48a62dd6a
Matteus Moreno <matteus_moreno@live.com>
2024-05-29 16:54:50 -0300

    inicio do projeto

---
+++ src/test/java/br/com/matteusmoreno/picpay_backend_challenge/PicpayBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.picpay_backend_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
